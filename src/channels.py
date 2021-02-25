0ea4042b5f56ef76a768ce1fc9ec55dc6418313f
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-02-25 23:50:31 +1100

    Added some more files for testing but they're empty

--- src/channels.py
+++ src/channels.py
@@ -1,20 +1,20 @@
 def channels_list(auth_user_id):
     return {
         'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
+            {
+                'channel_id': 1,
+                'name': 'My Channel',
+            }
         ],
     }
 
 def channels_listall(auth_user_id):
     return {
         'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
+            {
+                'channel_id': 1,
+                'name': 'My Channel',
+            }
         ],
     }
 
