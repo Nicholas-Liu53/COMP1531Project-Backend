c07d1e392c15e5c7bce64e616ac7a9d1ed2c3627
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-03-01 23:55:24 +1100

    just to pull 2.0

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
 # File to test functions in src/auth.py
 
 import pytest
-from src.auth import *
+from src.auth import auth_login_v1, auth_register_v1
 
 def test_auth_login():
     pass
--- tests/test_channels.py
+++ tests/test_channels.py
@@ -1,7 +1,7 @@
 # File to test functions in src/channels.py
 
 import pytest
-from src.channels import *
+from src.channels import channels_list_v1, channels_listall_v1, channels_create_v1
 
 def test_channels_list():
     pass
