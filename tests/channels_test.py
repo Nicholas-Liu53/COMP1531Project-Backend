e5dce8af4feeb06c6d1a85f555738eb35f99b2b9
z5310204 <z5310204@cse.unsw.edu.au>
2021-03-02 17:33:58 +1100

    Added some basic tests in comments

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -5,9 +5,20 @@ from src.channels import channels_list_v1, channels_listall_v1, channels_create_
 import src.auth, src.channel
 
 def test_channels_list():
+    # Test 1: For an empty data file, test if running the function with any auth_user_id returns anything
+    ## Should raise an AccessError
+    # Test 2: Add users but don't join any channels. Test if running the function with their auth_user_id returns anything
+    # Test 3: Add users to the channel and test if running the function returns the correct input
+    # Test 4: Ensure that the function returns the correct data type
+    
     pass
 
 def test_channels_listall():
+    # Test 1: If a user with an invalid auth_user_id is inputed, an AccessError is raised
+    # Test 2: Ensure that the function returns the correct data type
+    # Test 3: Ensure that both public and private channels are displayed
+    # Test 4: Add users but don't join any channels. Test if running the function with their auth_user_id returns anything
+    # Test 5: Add users to the channel and test if running the function returns the correct input
     pass
 
 def test_channels_create():
