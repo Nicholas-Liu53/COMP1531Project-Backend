98ce7dd12e39345f1a08f4d27325cbbe216efcfb
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-03-02 18:00:43 +1100

    wrote testing up for channel_join and channels_create in respective test files

--- src/other.py
+++ src/other.py
@@ -1,5 +1,25 @@
+import src.data
+
 def clear_v1():
-    pass
+
+    data.users = [
+        {
+            'user_id': None,
+            'first_name': None,
+            'last_name': None,
+            'email': None,
+            'password': None
+        }
+    ]
+
+    data.channels = [
+        {
+            'channel_id': None,
+            'channel_name': None,
+            'owner_members': [],
+            'all_members': []
+        }
+    ]
 
 def search_v1(auth_user_id, query_str):
     return {
--- tests/channels_test.py
+++ tests/channels_test.py
@@ -2,7 +2,7 @@
 
 import pytest
 from src.channels import channels_list_v1, channels_listall_v1, channels_create_v1
-import src.auth, src.channel
+import src.auth, src.channel, src.other
 
 def test_channels_list():
     pass
@@ -11,28 +11,32 @@ def test_channels_listall():
     pass
 
 def test_channels_create():
-    #? Setup users and create shorthand for strings for testing code
-    userID1 = auth.auth_register_v1("ayelmao@gmail.com", "Bl00dO4th", "C", "L")
-    userID2 = auth.auth_register_v1("lolrofl@gmail.com", "pr3ttynAme", "S", "S")
+    #* Setup users and create shorthand for strings for testing code
+    userID1 = src.auth.auth_register_v1("ayelmao@gmail.com", "Bl00dO4th", "C", "L")
+    userID2 = src.auth.auth_register_v1("lolrofl@gmail.com", "pr3ttynAme", "S", "S")
     
-    uID   = 'auth_user_id'
+    AuID   = 'auth_user_id'
     cID   = 'channel_id'
     chans = 'channels'
 
     # Test 1: Newly created public channel by userID1 appears in both of his channel list
-    firstChannel = channels_create_v1(userID1[uID], 'Oogway', True)
-    assert {cID: firstChannel, 'name': 'Oogway'} is in channels_list_v1(userID1[uID])[chans]
-    assert {cID: firstChannel, 'name': 'Oogway'} is in channels_listall_v1(userID1[uID])[chans]
+    firstChannel = channels_create_v1(userID1[AuID], 'Oogway', True)
+    assert {cID: firstChannel[cID], 'name': 'Oogway'} in channels_list_v1(userID1[AuID])[chans]
+    assert {cID: firstChannel[cID], 'name': 'Oogway'} in channels_listall_v1(userID1[AuID])[chans]
 
     # Test 2: Make sure this channel doesn't appear in userID2's channel list, but does in listall
-    assert {cID: firstChannel, 'name': 'Oogway'} is not in channels_list_v1(userID2[uID])[chans]
-    assert {cID: firstChannel, 'name': 'Oogway'} is in channels_listall_v1(userID2[uID])[chans]
+    assert {cID: firstChannel[cID], 'name': 'Oogway'} not in channels_list_v1(userID2[AuID])[chans]
+    assert {cID: firstChannel[cID], 'name': 'Oogway'} in channels_listall_v1(userID2[AuID])[chans]
 
     # Test 3: Newly created private channel by userID2 appears in his channel list
-    secondChannel = channels_create_v1(userID2[uID], 'Yayot', False)
-    assert {cID: secondChannel, 'name': 'Yayot'} is in channels_list_v1(userID2[uID])[chans]
-    assert {cID: secondChannel, 'name': 'Yayot'} is in channels_listall_v1(userID2[uID])[chans]
+    secondChannel = channels_create_v1(userID2[AuID], 'Yayot', False)
+    assert {cID: secondChannel[cID], 'name': 'Yayot'} in channels_list_v1(userID2[AuID])[chans]
+    assert {cID: secondChannel[cID], 'name': 'Yayot'} in channels_listall_v1(userID2[AuID])[chans]
 
     # Test 4: Make sure this channel doesn't appear in either of userID1's channel lists
-    assert {cID: secondChannel, 'name': 'Yayot'} is not in channels_list_v1(userID1[uID])[chans]
-    assert {cID: secondChannel, 'name': 'Yayot'} is not in channels_listall_v1(userID1[uID])[chans]
+    assert {cID: secondChannel[cID], 'name': 'Yayot'} not in channels_list_v1(userID1[AuID])[chans]
+    assert {cID: secondChannel[cID], 'name': 'Yayot'} not in channels_listall_v1(userID1[AuID])[chans]
+
+    #* Finished testing for this function
+    #! Clearing data
+    src.other.clear_v1()
