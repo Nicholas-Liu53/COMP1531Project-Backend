1c4d67ce8d06015aa06b98637193fd0cffffd490
z5310204 <z5310204@cse.unsw.edu.au>
2021-03-03 11:07:33 +1100

    Implemented channel_list_all_v1

--- src/channels.py
+++ src/channels.py
@@ -1,4 +1,5 @@
 import src.data
+from src.error import InputError, AccessError
 
 def channels_list_v1(auth_user_id):
     return {
@@ -11,16 +12,27 @@ def channels_list_v1(auth_user_id):
     }
 
 def channels_listall_v1(auth_user_id):
-    return {
-        'channels': [
-            {
-                'channel_id': 1,
-                'name': 'My Channel',
-            }
-        ],
-    }
+    # First, check if auth_user_id is a valid user_id
+    try:
+        check_auth_user_id(auth_user_id)
+    except AccessError:
+        print("Access error, please try again")
+
+    # If auth_user_id is valid, then it should print all channels in data
+    return src.data.channels
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {
         'channel_id': 1,
     }
+
+# Function that checks if auth_user_id is valid
+def check_auth_user_id(auth_user_id):
+    
+    uID = 'user_id'
+    for dictionary in src.data.users:
+        if auth_user_id == dictionary[uID]:
+            return
+    
+    raise AccessError
+
