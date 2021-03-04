1d5424bdbc8bfb46902edb5b591679971d81dc21
z5310204 <z5310204@cse.unsw.edu.au>
2021-03-05 08:31:34 +1100

    Started implementation of channels_list_v1

--- src/channels.py
+++ src/channels.py
@@ -2,15 +2,19 @@ import src.data
 from src.error import InputError, AccessError
 
 def channels_list_v1(auth_user_id):
-    return {
-        'channels': [
-            {
-                'channel_id': 1,
-                'name': 'My Channel',
-            }
-        ],
-    }
+    # First, check if auth_user-id is a valid user_id
+    try:
+        check_auth_user_id(auth_user_id)
+    except AccessError:
+        print("Access error, please try again")
+    
+    output = []
+    # Find channels that user is part of and add them to the output list
+    for dictionary in src.data.channels:
+        if auth_user_id in dictionary['all_members']:
+            output.append(dictionary)
 
+    return {'channels': output}
 def channels_listall_v1(auth_user_id):
     # First, check if auth_user_id is a valid user_id
     try:
@@ -19,7 +23,7 @@ def channels_listall_v1(auth_user_id):
         print("Access error, please try again")
 
     # If auth_user_id is valid, then it should print all channels in data
-    return src.data.channels
+    return {src.data.channels}
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {
@@ -33,6 +37,5 @@ def check_auth_user_id(auth_user_id):
     for dictionary in src.data.users:
         if auth_user_id == dictionary[uID]:
             return
-    
     raise AccessError
 
