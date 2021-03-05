3730984ddfcc085f1e6a256b1183838f60bb6740
z5311921 <z5311921@cse.unsw.edu.au>
2021-03-05 17:23:40 +1100

    Fixed the expected output of user_id to an integer and finished implementation of auth_register_v1 and auth_login_v1

--- src/auth.py
+++ src/auth.py
@@ -1,25 +1,92 @@
 import src.data
 from src.error import AccessError, InputError
+import re
+
+def clear_v1():
+    src.data.users = []
 
 def auth_login_v1(email, password):
-    try:
-        for user in src.data.users:
-            if email == user[email] and password == user['password']:
-                return {
-                    'auth_user_id': user['user_id'],
-                }
-    except: Exception(InputError) 
+    for user in src.data.users:
+        if email == user['email'] and password == user['password']:
+            return {
+                'auth_user_id': user['user_id'],
+            }
+    raise InputError    
 
 
 
 def auth_register_v1(email, password, name_first, name_last):
+    # setting all characters to lowercase
+    name_first = name_first.lower()
+    name_last = name_last.lower()
+
+    # removing whitespace from name_first and name_last
+    name_first = name_first.split()
+    name_first = ''.join(name_first)
+    name_last = name_last.split()
+    name_last = ''.join(name_last)
+    
+    # removing the '@' from name_first and name_last
+    name_first = name_first.replace("@", "")
+    name_last = name_last.replace("@", "")
+    
+    # checking if inputted email is valid
+    if re.search('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email):
+        pass    
+    else: 
+        raise InputError
+
+    # checking if inputted email is already being used by another user
+    for user in src.data.users:
+        if email == user['email']:
+            raise InputError
+
+    # checking if password is valid 
+    if len(password) < 6:
+        raise InputError        
+
+    # checking if first_name ranges between 1 - 50
+    if len(name_first) < 1 or len(name_first) > 50:
+        raise InputError
+
+     # checking if last_name ranges between 1 - 50
+    if len(name_last) < 1 or len(name_last) > 50:
+        raise InputError
+
+    # constructing the user_id from first and last name     
+    # checking for total length of first and last name 
+    if len(name_first) > 20:
+        handle_string = name_first[0:21]
+        
+    elif len(name_first) + len(name_last) > 20:
+        cutoff_len = 20 - len(name_first)
+        handle_string = name_first + name_last[0:cutoff_len]
+    else:    
+        handle_string = name_first + name_last
+    
+    trailing_int = 0 
+    
+    # checking for duplicated names   
+    for user in src.data.users:
+        if handle_string == user['handle_string']:
+            if trailing_int > 0:
+                handle_string = handle_string[0:-1] + str(trailing_int)
+            else:
+                handle_string = handle_string + str(trailing_int)
+            trailing_int += 1
+
+    user_id = len(src.data.users)
+
     src.data.users.append({
         'email' : email,
         'password' : password,
         'name_first' : name_first,
         'name_last' : name_last,
-        'user_id' : name_first + name_last
+        'user_id' : user_id,
+        'handle_string' : handle_string,
     })
     return {
-        'auth_user_id': name_first + name_last,
+        'auth_user_id': user_id,
     }
+    
+    
--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,100 +1,122 @@
 # File to test functions in src/auth.py
 from src.error import AccessError, InputError
 import pytest
-from src.auth import auth_login_v1, auth_register_v1
+from src.auth import auth_login_v1, auth_register_v1, clear_v1
 import src.channel, src.channels
 
 def test_auth_login_valid():
+    clear_v1()
     return_val = auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman")
-    assert auth_login_v1("caricoleman@gmail.com", "1234567") == {'auth_user_id': "caricoleman",}
+    assert auth_login_v1("caricoleman@gmail.com", "1234567") == {'auth_user_id': 0,}
 
 def test_auth_login_valid_multiple():
+    clear_v1()
     return_val1 = auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman")
     return_val2 = auth_register_v1("ericamondy@gmail.com", "1234567", "erica", "mondy") 
 
-    assert auth_login_v1("caricoleman@gmail.com", "1234567") == {'auth_user_id': "caricoleman",}  
-    assert auth_login_v1("ericamondy@gmail.com", "1234567") == {'auth_user_id': "ericamondy",}
+    assert auth_login_v1("caricoleman@gmail.com", "1234567") == {'auth_user_id': 0,}  
+    assert auth_login_v1("ericamondy@gmail.com", "1234567") == {'auth_user_id': 1,}
 
 def test_auth_login_invalid_email():
+    clear_v1()
     with pytest.raises(InputError):
         return_val = auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman")
         auth_login_v1("caricoleman.com", "1234567")
 
 def test_auth_login_invalid_not_registered_email():
+    clear_v1()
     with pytest.raises(InputError):
         return_val = auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman")
         auth_login_v1("caricolema@gmail.com", "1234567") 
         auth_login_v1("ericamondy@gmail.com", "1234567")  
 
 def test_auth_login_invalid_incorrect_password():
+    clear_v1()
     with pytest.raises(InputError):
         return_val = auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman")
         auth_login_v1("caricoleman@gmail.com", "12345")
 
 def test_auth_register_valid():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman",}
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': 0,}
 
 def test_auth_register_valid_multiple():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman",}
-    assert auth_register_v1("ericamondy@gmail.com", "1234567", "erica", "mondy") == {'auth_user_id': "ericamondy",}
-    assert auth_register_v1("hilarybently@gmail.com", "1234567", "hilary", "bently") == {'auth_user_id': "hilarybently",}
-    assert auth_register_v1("kentonwatkins@gmail.com", "1234567", "kenton", "watkins") == {'auth_user_id': "kentonwatkins",}
-    assert auth_register_v1("caludiamarley@gmail.com", "1234567", "claudia", "marley") == {'auth_user_id': "claudiamarley",}    
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': 0,}
+    assert auth_register_v1("ericamondy@gmail.com", "1234567", "erica", "mondy") == {'auth_user_id': 1,}
+    assert auth_register_v1("hilarybently@gmail.com", "1234567", "hilary", "bently") == {'auth_user_id': 2,}
+    assert auth_register_v1("kentonwatkins@gmail.com", "1234567", "kenton", "watkins") == {'auth_user_id': 3,}
+    assert auth_register_v1("caludiamarley@gmail.com", "1234567", "claudia", "marley") == {'auth_user_id': 4,}    
 
 def test_auth_register_valid_same_name():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman",}
-    assert auth_register_v1("caricoleman@hotmail.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman1",}
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': 0,}
+    assert auth_register_v1("caricoleman@hotmail.com", "1234567", "cari", "coleman") == {'auth_user_id': 1,}
 
 def test_auth_register_valid_same_name_multiple():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman",}
-    assert auth_register_v1("caricoleman@hotmail.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman0",}
-    assert auth_register_v1("caricoleman@yahoo.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman1",}
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': 0,}
+    assert auth_register_v1("caricoleman@hotmail.com", "1234567", "cari", "coleman") == {'auth_user_id': 1,}
+    assert auth_register_v1("caricoleman@yahoo.com", "1234567", "cari", "coleman") == {'auth_user_id': 2,}
 
 def test_auth_register_valid_front_capatilised():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "Cari", "Coleman") == {'auth_user_id': "caricoleman", }
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "Cari", "Coleman") == {'auth_user_id': 0, }
     
 def test_auth_register_valid_random_capatilised():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "CaRi", "coLemaN") == {'auth_user_id': "caricoleman", }
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "CaRi", "coLemaN") == {'auth_user_id': 0, }
 
 def test_auth_register_valid_whitespace():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", " cari", "coleman ") == {'auth_user_id': "caricoleman", }
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", " cari", "coleman ") == {'auth_user_id': 0, }
 
 def test_auth_register_valid_at_symbol():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari@", "coleman") == {'auth_user_id': "caricoleman", }
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari@", "coleman") == {'auth_user_id': 0, }
 
 def test_auth_register_valid_long_name():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cariiiiiiiiiiiiiii", "coleman") == {'auth_user_id': "cariiiiiiiiiiiiiiico",}
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cariiiiiiiiiiiiiii", "coleman") == {'auth_user_id': 0,}
 
 def test_auth_register_valid_long_name_multiple():
-    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cariiiiiiiiiiiiiii", "coleman") == {'auth_user_id': "cariiiiiiiiiiiiiiico", }
-    assert auth_register_v1("caricoleman@hotmail.com", "1234567", "cariiiiiiiiiiiiiii", "coleman") == {'auth_user_id': "cariiiiiiiiiiiiiiico0", }
+    clear_v1()
+    assert auth_register_v1("caricoleman@gmail.com", "1234567", "cariiiiiiiiiiiiiii", "coleman") == {'auth_user_id': 0, }
+    assert auth_register_v1("caricoleman@hotmail.com", "1234567", "cariiiiiiiiiiiiiii", "coleman") == {'auth_user_id': 1, }
 
 def test_auth_register_invalid_email():
+    clear_v1()
     with pytest.raises(InputError):
         auth_register_v1("caricoleman.com", "1234567", "cari", "coleman") 
     
 def test_auth_register_invalid_same_email():
+    clear_v1()
     with pytest.raises(InputError):
-        assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': "caricoleman",}
-        auth_register_v1("caricoleman@yahoo.com", "1234567", "erica", "mondy")
+        assert auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "coleman") == {'auth_user_id': 0,}
+        auth_register_v1("caricoleman@gmail.com", "1234567", "erica", "mondy")
 
 def test_auth_register_invalid_password():
+    clear_v1()
     with pytest.raises(InputError):
         auth_register_v1("caricoleman@gmail.com", "1234", "cari", "coleman") 
 
 def test_auth_register_invalid_long_first_name():
+    clear_v1()
     with pytest.raises(InputError):
         auth_register_v1("caricoleman@gmail.com", "1234567", "cariiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", "coleman") 
 
 def test_auth_register_invalid_long_last_name():
+    clear_v1()
     with pytest.raises(InputError):
         auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "colemaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan") 
 
 def test_auth_register_invalid_no_first_name():
+    clear_v1()
     with pytest.raises(InputError):
         auth_register_v1("caricoleman@gmail.com", "1234567", "", "coleman") 
 
 def test_auth_register_invalid_no_last_name():
+    clear_v1()
     with pytest.raises(InputError):
         auth_register_v1("caricoleman@gmail.com", "1234567", "cari", "") 
 
