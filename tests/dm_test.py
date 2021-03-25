91949aef220890f699b0a9926aadda6b69f37279
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-03-25 19:33:59 +1100

    Finished testing for first 3 functions

--- tests/dm_test.py
+++ tests/dm_test.py
@@ -22,54 +22,65 @@ SECRET = 'MENG'
 
 def test_dm_details_valid():
     src.other.clear_v1()
-    '''
-    <Register 2 users>
-    <Create DM with both users>
-    <Both users call function, make sure expected output is correct>
-    '''
-    pass
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    dm1 = dm_create_v1(user1[token], [user2[AuID]])
+    expected = {
+        dmID: 0,
+        Name: 'jonahfromtonga, steveirwin',
+    }
+
+    assert dm_details_v1(user1[token], dm1[dmID]) == expected
+    assert dm_details_v1(user2[token], dm1[dmID]) == expected
 
 def test_dm_details_errors():
     src.other.clear_v1()
-    #* InputError - dm_id is not a valid DM
-    '''
-    <Register 2 users>
-    <Attempt to create a DM with both users>
-    '''
+    invalid_dm_id = -1
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+
+    with pytest.raises(InputError):
+        dm_details_v1(user1[token], invalid_dm_id)
 
     src.other.clear_v1()
-    #* AccessError - authorised user is not part of this DM
-    '''
-    <Register 3 users>
-    <Create DM with first 2 users>
-    <3rd user attempts to call function>
-    '''
-    pass
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    user3 = src.auth.auth_register_v1("third@gmail.com", "password", "Rock", "Sand")
+    dm1 = dm_create_v1(user1[token], [user2[AuID]])
+
+    with pytest.raises(AccessError):
+        dm_details_v1(user3[token], dm1[dmID])
 
 def test_dm_list():
     src.other.clear_v1()
-    #* User is not part of any DM
-    '''
-    < Register 3 users >
-    < Create DM with first 2 users >
-    < Call the function with third user>
-    '''
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    user3 = src.auth.auth_register_v1("third@gmail.com", "password", "Rock", "Sand")
+    dm_create_v1(user1[token], [user2[AuID]])
+    
+    assert dm_list_v1(user3[token]) == {'dms': []}
+    
     src.other.clear_v1()
-    #* User is part of the only DM
-    '''
-    < Register 2 users >
-    < Create DM with both users >
-    < Both users call function, make sure expected output is correct >
-    '''
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    dm_create_v1(user1[token], [user2[AuID]])
+    
+    assert dm_list_v1(user3[token]) == {'dms': [{
+        dmID: 0,
+        Name: 'jonahfromtonga, steveirwin',
+    }]}
+
     src.other.clear_v1()
-    #* User is part of some of the DMs
-    '''
-    < Register 3 users >
-    < Create DM with all users >
-    < Create DM with only first 2 >
-    < Call the function with third user>
-    '''
-    pass
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    user3 = src.auth.auth_register_v1("third@gmail.com", "password", "Rock", "Sand")
+    dm_create_v1(user1[token], [user2[AuID]])
+    dm_create_v1(user1[token], [user2[AuID], user3[AuID]])
+    
+    assert dm_list_v1(user3[token]) == {'dms': [{
+        dmID: 1,
+        Name: 'jonahfromtonga, steveirwin',
+    }]}
 
 def test_dm_create_valid():
     src.other.clear_v1()
@@ -135,7 +146,7 @@ def test_dm_name():
 
     assert dm1[Name] == 'jonahfromtonga, steveirwin'
 
-    dm_invite_v1(user1[token], dm1[dmID], user3[uID])
+    dm_invite_v1(user1[token], dm1[dmID], user3[AuID])
     result1 = dm_details_v1(user1[token], dm1[dmID])
     
     assert result1[Name] == 'jonahfromtonga, steveirwin'
@@ -171,9 +182,9 @@ def test_dm_unauthorised_user():
     src.other.clear_v1()
     user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Hotel?", "Trivago")
     user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Hotel?", "Trivago")
-    dm1 = dm_create_v1(user1[token], [user2[uID]])
+    dm1 = dm_create_v1(user1[token], [user2[AuID]])
 
     with pytest.raises(AccessError):
         dm_details_v1(removedUser[token], dm1[dm_id])
         dm_list_v1(removedUser[token])
-        dm_create_v1(removedUser[token], [user1[uID]])
+        dm_create_v1(removedUser[token], [user1[AuID]])
