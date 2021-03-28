21485769750a2fb32899b1602bb57ab31f5e28ec
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-03-28 18:09:38 +1100

    Implemented senddm with Ethan

--- src/message.py
+++ src/message.py
@@ -1,3 +1,24 @@
+import src.data
+from src.error import AccessError, InputError
+import src.auth
+from datetime import datetime
+import jwt
+
+SECRET = 'MENG'
+
+AuID      = 'auth_user_id'
+uID       = 'u_id'
+cID       = 'channel_id'
+creatorID = 'creator_id'
+allMems   = 'all_members'
+Name      = 'name'
+fName     = 'name_first'
+lName     = 'name_last'
+chans     = 'channels'
+handle    = 'handle_string'
+dmID      = 'dm_id'
+seshID    = 'session_id'
+
 def message_send_v1(auth_user_id, channel_id, message):
     return {
         'message_id': 1,
@@ -12,4 +33,45 @@ def message_edit_v1(auth_user_id, message_id, message):
     }
 
 def message_senddm_v1(token, dm_id, message):
-    pass
+    auth_user_id, _ = decode(token)
+    _, dmMembers = get_members(-1, dm_id)
+    if auth_user_id not in dmMembers:
+        raise AccessError
+
+    message_id = len(src.data.messages_log)
+    now = datetime.now
+    time_created = int(now.strftime("%s"))
+    
+    return {
+        cID: -1,
+        dmID: dm_id,
+        'message_id': message_id,
+        uID: auth_user_id,
+        'message': message, 
+        'time_created': time_created,
+    }
+
+def decode(token):
+    payload = jwt.decode(token, SECRET, algorithms='HS256')
+    auth_user_id, session_id = payload.get('session_id'), payload.get('user_id')
+    check_session(auth_user_id, session_id)
+    return auth_user_id, session_id
+
+def check_session(auth_user_id, session_id):
+    for user in src.data.users:
+        if auth_user_id == user[uID]:
+            if session_id in user[session_id]:
+                return
+    raise AccessError
+
+def get_members(channel_id, dm_id):
+    if dm_id == -1:
+        for chanDetails in src.data.channels:
+            if channel_id == chanDetails[cID]:
+                return chanDetails[Name], chanDetails[allMems]
+        raise InputError
+    else:
+        for dmDetails in src.data.dms:
+            if dm_id == dmDetails[dmID]:
+                return dmDetails[Name], dmDetails[allMems]
+        raise InputError
