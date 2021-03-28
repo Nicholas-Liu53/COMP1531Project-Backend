a0bafbf465f81e0c7c477ebc80a355122c46ea51
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-03-28 17:06:15 +1100

    Added testing for senddm

--- tests/message_test.py
+++ tests/message_test.py
@@ -1,30 +1,61 @@
 import pytest
-from src.message import message_send_v1, message_remove_v1, message_edit_v1
+from src.message import message_send_v1, message_remove_v1, message_edit_v1, message_senddm_v1
 from src.error import InputError, AccessError
-import src.channel, src.channels, src.auth
+import src.channel, src.channels, src.auth, src.dm
+import jwt
+
+AuID    = 'auth_user_id'
+uID     = 'u_id'
+cID     = 'channel_id'
+allMems = 'all_members'
+Name   = 'name'
+fName   = 'name_first'
+lName   = 'name_last'
+chans   = 'channels'
+token   = 'token'
+dmID    = 'dm_id'
+
+@pytest.fixture
+def invalid_token():
+    return jwt.encode({'session_id': -1, 'user_id': -1}, SECRET, algorithm='HS256')
 
 #* Test send functions together with message/send/v2
 #? Test if message_id increases correctly
 
 def test_senddm_errors():
-    '''
-    #* Test if a authorised user who is not part of the DM they are posting to raises an Access Error
-    < Register 3 users >
-    < Create dm with 2 users >
-    < Try send a message with 3rd user >
-    #* Test if passing an invalid dm_id raises an InputError
-    < Register user >
-    < Try send a message with user >
-    #* Test if message is more than 1000 characters rauses an InputError
-    < Register 2 users >
-    < Create dm with both users >
-    < Try send a long message >
-    '''
-    pass
+    src.other.clear_v1()
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    user3 = src.auth.auth_register_v1("third@gmail.com", "password", "Rock", "Sand")
+    dm1 = src.dm.dm_create_v1(user1[token], [user2[AuID]])
+    
+    with pytest.raises(AccessError):
+        message_senddm_v1(user3[token], dm1[dmID], '')
+
+    src.other.clear_v1()
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    invalid_dm_id = -1
+
+    with pytest.raises(InputError):
+        message_senddm_v1(user1[token], invalid_dm_id, '')
+
+    message = ''
+    for _ in range(1500):
+        message += 'a'
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    dm1 = src.dm.dm_create_v1(user1[token], [user2[AuID]])
+    with pytest.raises(InputError):
+        message_senddm_v1(user1[token], dm1[dmID], message)
 
 def test_senddm_multiple():
     pass
 
-def test_dm_unauthorised_user():
+def test_dm_unauthorised_user(invalid_token):
     #* All unauthorised user tests
-    pass
+    user1 = src.auth.auth_register_v1("first@gmail.com", "password", "Steve", "Irwin")
+    user2 = src.auth.auth_register_v1("second@gmail.com", "password", "Jonah", "from Tonga")
+    dm1 = src.dm.dm_create_v1(user1[token], [user2[AuID]])
+    
+    with pytest.raises(AccessError):
+        message_senddm_v1(invalid_token, dm1[dmID], '')
