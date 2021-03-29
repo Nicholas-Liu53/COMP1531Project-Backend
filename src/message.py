7810b598a40f2be12c2a0bef6b7f2d35a8e61ce6
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-03-29 23:39:21 +1100

    Added testing and implementation for message.py

--- src/message.py
+++ src/message.py
@@ -1,8 +1,37 @@
 import src.data
 from src.error import AccessError, InputError
 from src.other import decode, get_channel, get_members, get_user
+from datetime import timezone, datetime
 
-def message_send_v1(auth_user_id, channel_id, message):
+def message_send_v1(token, channel_id, message):
+    
+    # Decode the token
+    auth_user_id, _ = decode(token)
+
+    # If the message is too long, raise InputError
+    if len(message) > 1000:
+        raise InputError
+
+    # Check if user is in channel
+    if auth_user_id not in get_channel(channel_id)['all_members']:
+        raise AccessError
+
+    now = datetime.now()
+    time_created = int(now.strftime("%s"))
+
+    # User is in the channel (which exists) & message is appropriate length
+    #* Time to send a message
+    data.messages_log.append(
+        {
+            'channel_id'    : channel_id,
+            'dm_id'         : -1,
+            'handle_string' : get_user(auth_user_id)['handle_string'],
+            'time_created'  : time_created,
+            'message_id'    : len(data.messages_log),
+            'message_string': message,
+        }
+    )
+
     return {
         'message_id': 1,
     }
--- tests/message_test.py
+++ tests/message_test.py
@@ -2,11 +2,51 @@ import pytest
 from src.message import message_send_v1, message_remove_v1, message_edit_v1
 from src.error import InputError, AccessError
 import src.channel, src.channels, src.auth
+from src.other import clear_v1
+
+AuID    = 'auth_user_id'
+uID     = 'u_id'
+cID     = 'channel_id'
+allMems = 'all_members'
+cName   = 'name'
+fName   = 'name_first'
+lName   = 'name_last'
+chans   = 'channels'
 
 # message_send_v1
 # When message is >1000 characters, InputError is raised
 # When authorised user is not part of the channel that they are trying to post in, AccessError is raised
 # After the function is sucessfully run, ensure that the return value is correct
+def test_message_send():
+    #* Ensure database is empty
+    #! Clearing data
+    src.other.clear_v1()
+
+    #* Setup users and channels and create shorthand for strings for testing code
+    userID1 = src.auth.auth_register_v1("ayelmao@gmail.com", "Bl00dO4th", "C", "L")
+    userID2 = src.auth.auth_register_v1("lolrofl@gmail.com", "pr3ttynAme", "S", "S")
+    userID3 = src.auth.auth_register_v1("zodiac@gmail.com", "T3dCruz", "T", "C")
+    userID4 = src.auth.auth_register_v1("ocasio@gmail.com", "Alex4ndr1a", "A", "O")
+
+    # userID1 made public channel 'TrumpPence'
+    firstChannel = src.channels.channels_create_v1(userID1[AuID], 'TrumpPence', True)
+
+    #* userID2 and userID3 join public channel 'TrumpPence'
+    channel_join_v1(userID2[AuID], firstChannel[cID])
+    channel_join_v1(userID3[AuID], firstChannel[cID])
+
+    #* Test if a super large message raises an InputError
+    for _ in range(1500):
+        message += '?'
+    with pytest.raises(InputError):
+        message_send_v1(userID1[token], firstChannel[cID], message)
+
+    #* Test if a user not in the channel tries to send a message into the channel
+    with pytest.raises(AccessError):
+        message_send_v1(userID4[token], firstChannel[cID], '?')
+
+    #! Clearing data
+    src.other.clear_v1()
 
 # message_edit_v1
 # When message is >1000 characters, InputError is raised
