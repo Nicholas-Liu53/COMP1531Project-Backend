f2a8debf7c9c7acc6b172735c2a7c1e7f2f4f747
Ethan Kwong <ethankwong@Ethans-MacBook-Air.local>
2021-04-02 23:43:19 +1100

    Fixing up admin test pylint

--- tests/admin_test.py
+++ tests/admin_test.py
@@ -2,10 +2,10 @@
 import pytest
 from src.admin import user_remove_v1, userpermission_change_v1, notifications_get_v1
 from src.error import AccessError, InputError
+from src.other import SECRET
 import src.channel, src.channels, src.auth, src.dm, src.message, src.other
 import jwt
 import json
-from src.other import SECRET
 
 AuID    = 'auth_user_id'
 uID     = 'u_id'
