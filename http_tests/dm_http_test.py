0b94b9da473ee1bc0dd4c3c8be0bb59bbcaa8ad5
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-04-03 20:20:24 +1100

    Wrote http tests for dm/details/v1

--- http_tests/dm_http_test.py
+++ http_tests/dm_http_test.py
@@ -1,51 +1,84 @@
 import pytest
-import re
-from subprocess import Popen, PIPE
-import signal
-from time import sleep
 import requests
+import src.other, src.auth
+from src.dm import dm_create_v1
+from src import config
 
+AuID    = 'auth_user_id'
+uID     = 'u_id'
+cID     = 'channel_id'
+allMems = 'all_members'
+Name    = 'name'
+dmName  = 'dm_name'
+fName   = 'name_first'
+lName   = 'name_last'
+chans   = 'channels'
+token   = 'token'
+dmID    = 'dm_id'
+handle  = 'handle_string'
+
+#* Fixture that clears and registers the first user
 @pytest.fixture
-def url():
-    url_re = re.compile(r' \* Running on ([^ ]*)')
-    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
-    line = server.stderr.readline()
-    local_url = url_re.match(line.decode())
-    if local_url:
-        yield local_url.group(1)
-        # Terminate the server
-        server.send_signal(signal.SIGINT)
-        waited = 0
-        while server.poll() is None and waited < 5:
-            sleep(0.1)
-            waited += 0.1
-        if server.poll() is None:
-            server.kill()
-    else:
-        server.kill()
-        raise Exception("Couldn't get URL from local server")
+def user1():
+    src.other.clear_v1()    
+    return src.auth.auth_register_v2("first@gmail.com", "password", "User", "1")
 
-# Simple sanity test to check that your server is set up properly
-def test_url(url):
-    assert url.startswith("http")
+#* Fixture that registers a second user
+@pytest.fixture
+def user2():
+    return src.auth.auth_register_v2("second@gmail.com", "password", "User", "2")
 
-def test_dm_details(url):
+#* Fixture that registers a third user
+@pytest.fixture
+def user3():
+    return src.auth.auth_register_v2("third@gmail.com", "password", "User", "3")
+
+def test_dm_details_empty(user1, user2):
+    dm1 = dm_create_v1(user1[token], [user2[AuID]])
+    expected = {
+        Name: 'user1, user2',
+        'members': [{
+            uID: user1[AuID], 
+            fName: "User",
+            lName: '1',
+            'email': 'first@gmail.com',
+            handle: 'user1',
+        }, {
+            uID: user2[AuID], 
+            fName: "User",
+            lName: '2',
+            'email': 'second@gmail.com',
+            handle: 'user2',
+        }
+        ]
+    }
+    assert requests.get(f"{config.url}dm/details/v1", params = {'token': user1[token], 'dm_id': dm1[dmID]}) == expected
+    assert requests.get(f"{config.url}dm/details/v1", params = {'token': user1[token], 'dm_id': dm1[dmID]}) == expected
+
+def test_dm_details_invalid_dm_id(user1):
+    invalid_dmID = -2
+    response = requests.get(f"{config.url}dm/details/v1", params = {'token': user1[token], 'dm_id': invalid_dmID})
+    assert response.status_code == 400
+
+def test_dm_details_not_in_dm(user1, user2, user3):
+    dm1 = dm_create_v1(user1[token], [user2[AuID]])
+    response = requests.get(f"{config.url}dm/details/v1", params = {'token': user3[token], 'dm_id': dm1[dmID]})
+    assert response.status_code == 403
+
+def test_dm_list():
     pass
 
-def test_dm_list(url):
+def test_dm_create():
     pass
 
-def test_dm_create(url):
+def test_dm_remove():
     pass
 
-def test_dm_remove(url):
+def test_dm_invite():
     pass
 
-def test_dm_invite(url):
+def test_dm_leave():
     pass
 
-def test_dm_leave(url):
-    pass
-
-def test_dm_messages(url):
+def test_dm_messages():
     pass
