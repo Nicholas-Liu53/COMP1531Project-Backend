5c3698c1f19db438f665531bba5db6bfd8f57d50
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-04-03 14:10:50 +1100

    Pushing coverage fixing for tigervnc

--- src/channel.py
+++ src/channel.py
@@ -286,18 +286,14 @@ def channel_join_v1(token, channel_id):
     auth_user_id, _ = decode(token)
 
     # Time to find the user details
-    userFound = False
+    try:
+        get_user(auth_user_id)
+    except:
+        raise AccessError
     j = 0
-    while not userFound:
-        if j >= len(src.data.users):
-            # If user doesn't exist in database, AccessError
-            raise AccessError
-        elif src.data.users[j]['u_id'] == auth_user_id:
-            userFound = True
+    while src.data.users[j]['u_id'] != auth_user_id:
         j += 1
 
-    j -= 1      # Undo extra increment
-    
     if src.data.channels[i]['is_public'] == False and src.data.users[j]['permission_id'] != 1:
         # If channel is private, AccessError
         raise AccessError
