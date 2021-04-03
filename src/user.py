dc8d2cf050909d5a9e222dff793b4eb9f8ee6fd2
z5311921 <z5311921@cse.unsw.edu.au>
2021-04-03 18:46:59 +1100

    Fixed user.py for 10/10 pylint

--- src/user.py
+++ src/user.py
@@ -4,14 +4,14 @@ import re
 from src.other import decode, check_session, get_user
 
 def user_profile_v2(token, u_id):
-    auth_user_id, session_id = decode(token)
+    decode(token)
 
     return {
         'user': get_user(u_id)
     }
 
 def user_setname_v2(token, name_first, name_last):
-    auth_user_id, session_id = decode(token)
+    auth_user_id = decode(token)
     
     if len(name_first) > 50 or len(name_first) < 1:
         raise InputError
@@ -26,7 +26,7 @@ def user_setname_v2(token, name_first, name_last):
     }
 
 def user_setemail_v2(token, email):
-    auth_user_id, session_id = decode(token)
+    auth_user_id = decode(token)
     
     if not re.search('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email):
         raise InputError
@@ -43,7 +43,7 @@ def user_setemail_v2(token, email):
     }
 
 def user_sethandle_v2(token, handle_str):
-    auth_user_id, session_id = decode(token)
+    auth_user_id = decode(token)
     
     if len(handle_str) < 3 or len(handle_str) > 20:
         raise InputError
@@ -60,7 +60,7 @@ def user_sethandle_v2(token, handle_str):
     }
 
 def users_all(token):
-    auth_user_id, session_id = decode(token)
+    decode(token)
     
     user_list = []
     
