0ef45aa03f113ae3db0a9bbe5a83adcb99e51562
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-04-05 00:29:38 +1000

    Added stub code for notifications http testing

---
+++ http_tests/notifications_http_test.py
@@ -0,0 +1,91 @@
+import pytest
+import requests
+import json
+from src.config import url
+from src.other import SECRET
+import jwt
+
+AuID    = 'auth_user_id'
+uID     = 'u_id'
+cID     = 'channel_id'
+allMems = 'all_members'
+Name    = 'name'
+dmName  = 'dm_name'
+fName   = 'name_first'
+lName   = 'name_last'
+chans   = 'channels'
+token   = 'token'
+dmID    = 'dm_id'
+handle  = 'handle_string'
+ownMems = 'owner_members'
+
+#* Fixture that clears and registers the first user
+@pytest.fixture
+def user1():
+    requests.delete(f"{url}clear/v1")    
+    response = requests.post(f"{url}auth/register/v2", json={
+        "email": "first@gmail.com",
+        "password": "password",
+        "name_first": "User",
+        "name_last": "1"
+    })
+    return response.json()
+
+#* Fixture that registers a second user
+@pytest.fixture
+def user2():
+    response = requests.post(f"{url}auth/register/v2", json={
+        "email": "second@gmail.com",
+        "password": "password",
+        "name_first": "User",
+        "name_last": "2"
+    })
+    return response.json()
+
+#* Fixture that registers a third user
+@pytest.fixture
+def user3():
+    response = requests.post(f"{url}auth/register/v2", json={
+        "email": "third@gmail.com",
+        "password": "password",
+        "name_first": "User",
+        "name_last": "3"
+    })
+    return response.json()
+
+#* Fixture that registers a fourth user
+@pytest.fixture
+def user4():
+    response = requests.post(f"{url}auth/register/v2", json={
+        "email": "fourth@gmail.com",
+        "password": "password",
+        "name_first": "User",
+        "name_last": "4"
+    })
+    return response.json()
+
+#* Fixture that returns a JWT with invalid u_id and session_id
+@pytest.fixture
+def invalid_token():
+    return jwt.encode({'session_id': -1, 'user_id': -1}, SECRET, algorithm='HS256')
+
+def test_http_notifications_get_in_channels(user1, user2, user3, user4):
+    pass
+
+def test_http_notifications_dms_added(user1, user2, user3):
+    pass
+
+def test_http_valid_dm_tag(user1, user2):
+    pass
+
+def test_http_valid_dm_20_chars(user1, user2):
+    pass
+
+def test_http_dm_no_tag(user1, user2, user3):
+    pass
+
+def test_http_dm_20_notifs(user1, user2):
+    pass
+
+def test_http_dm_edit_notif(user1, user2):
+    pass
