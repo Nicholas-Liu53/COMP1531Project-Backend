5461b12ad7f142d58a993b5050415250c819547e
Nicholas Liu <nicholas.liu1@student.unsw.edu.au>
2021-04-05 00:13:22 +1000

    Finished http testing for message_remove

--- http_tests/message_http_test.py
+++ http_tests/message_http_test.py
@@ -244,7 +244,93 @@ def test_http_message_edit(user1, user2, user3, user4):
     }).status_code == 403
 
 def test_http_message_remove(user1, user2, user3, user4):
-    pass
+    c1 = requests.post(f"{url}channels/create/v2", json={
+        "token": user2[token],
+        "name": "TrumpPence",
+        "is_public": True
+    })
+    requests.post(f"{url}channel/join/v2", json={
+        "token": user1[token],
+        "channel_id": c1.json()['channel_id']
+    })
+    requests.post(f"{url}channel/join/v2", json={
+        "token": user3[token],
+        "channel_id": c1.json()['channel_id']
+    })
+    requests.post(f"{url}channel/join/v2", json={
+        "token": user4[token],
+        "channel_id": c1.json()['channel_id']
+    })
+    m1 = requests.post(f"{url}message/send/v2", json={
+        "token": user3[token],
+        "channel_id": c1.json()['channel_id'],
+        "message": "Yo yo waz poppin'?"
+    }).json()
+    m2 = requests.post(f"{url}message/send/v2", json={
+        "token": user3[token],
+        "channel_id": c1.json()['channel_id'],
+        "message": "Huh?"
+    }).json()
+    m3 = requests.post(f"{url}message/send/v2", json={
+        "token": user3[token],
+        "channel_id": c1.json()['channel_id'],
+        "message": "John Cena"
+    }).json()
+    m4 = requests.post(f"{url}message/send/v2", json={
+        "token": user3[token],
+        "channel_id": c1.json()['channel_id'],
+        "message": "Ricegum"
+    }).json()
+    requests.delete(f"{url}message/remove/v1", json={
+        "token": user1[token],
+        "message_id": m1['message_id']
+    })
+    messageFound = False
+    for messageDict in requests.get(f"{url}channel/messages/v2", params={
+        "token": user1[token],
+        "channel_id": c1.json()['channel_id'],
+        "start": 0
+    }).json()['messages']:
+        if m1['message_id'] == messageDict['message_id']:
+            messageFound = True
+            break
+    assert messageFound is False
+    requests.delete(f"{url}message/remove/v1", json={
+        "token": user2[token],
+        "message_id": m2['message_id']
+    })
+    messageFound = False
+    for messageDict in requests.get(f"{url}channel/messages/v2", params={
+        "token": user2[token],
+        "channel_id": c1.json()['channel_id'],
+        "start": 0
+    }).json()['messages']:
+        if m2['message_id'] == messageDict['message_id']:
+            messageFound = True
+            break
+    assert messageFound is False
+    requests.delete(f"{url}message/remove/v1", json={
+        "token": user3[token],
+        "message_id": m3['message_id']
+    })
+    messageFound = False
+    for messageDict in requests.get(f"{url}channel/messages/v2", params={
+        "token": user3[token],
+        "channel_id": c1.json()['channel_id'],
+        "start": 0
+    }).json()['messages']:
+        if m3['message_id'] == messageDict['message_id']:
+            messageFound = True
+            break
+    assert messageFound is False
+    assert requests.delete(f"{url}message/remove/v1", json={
+        "token": user4[token],
+        "message_id": m4['message_id']
+    }).status_code == 403
+    assert requests.delete(f"{url}message/remove/v1", json={
+        "token": user4[token],
+        "message_id": -1
+    }).status_code == 400
 
 def test_http_message_share_todm(user1, user2, user3, user4):
     pass
