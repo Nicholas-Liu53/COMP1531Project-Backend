572c319d776427d2ba5ace29c572b5709b4800fa
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-04-04 12:11:24 +1000

    Finished 3 dm http tests

--- http_tests/dm_http_test.py
+++ http_tests/dm_http_test.py
@@ -2,7 +2,9 @@ import pytest
 import requests
 import src.other, src.auth
 from src.dm import dm_create_v1
-from src import config
+from src.config import url
+from src.other import SECRET
+import jwt
 
 AuID    = 'auth_user_id'
 uID     = 'u_id'
@@ -20,71 +22,127 @@ handle  = 'handle_string'
 #* Fixture that clears and registers the first user
 @pytest.fixture
 def user1():
-    src.other.clear_v1()    
-    return src.auth.auth_register_v2("first@gmail.com", "password", "User", "1")
+    requests.delete(f"{url}clear/v1")    
+    response = requests.post(f"{url}auth/register/v2", json={
+        "email": "first@gmail.com",
+        "password": "password",
+        "name_first": "User",
+        "name_last": "1"
+    })
+    return response.json()
 
 #* Fixture that registers a second user
 @pytest.fixture
 def user2():
-    return src.auth.auth_register_v2("second@gmail.com", "password", "User", "2")
+    response = requests.post(f"{url}auth/register/v2", json={
+        "email": "second@gmail.com",
+        "password": "password",
+        "name_first": "User",
+        "name_last": "2"}
+    )
+    return response.json()
 
 #* Fixture that registers a third user
 @pytest.fixture
 def user3():
-    return src.auth.auth_register_v2("third@gmail.com", "password", "User", "3")
+    response = requests.post(f"{url}auth/register/v2", json={
+        "email": "third@gmail.com",
+        "password": "password",
+        "name_first": "User",
+        "name_last": "3"}
+    )
+    return response.json()
 
-def test_dm_details_empty(user1, user2):
-    # dmResponse = requests.post(f"{config.url}dm/create/v1", json={
-    #     'token': user1[token],
-    #     'u_ids': [user2[AuID]]
-    # })
-    # dm1 = dmResponse.json()
-    # expected = {
-    #     Name: 'user1, user2',
-    #     'members': [{
-    #         uID: user1[AuID], 
-    #         fName: "User",
-    #         lName: '1',
-    #         'email': 'first@gmail.com',
-    #         handle: 'user1',
-    #     }, {
-    #         uID: user2[AuID], 
-    #         fName: "User",
-    #         lName: '2',
-    #         'email': 'second@gmail.com',
-    #         handle: 'user2',
-    #     }
-    #     ]
-    # }
-    # assert requests.get(f"{config.url}dm/details/v1", params = {'token': user1[token], 'dm_id': dm1[dmID]}) == expected
-    # assert requests.get(f"{config.url}dm/details/v1", params = {'token': user1[token], 'dm_id': dm1[dmID]}) == expected
-    pass
+#* Fixture that returns a JWT with invalid u_id and session_id
+@pytest.fixture
+def invalid_token():
+    return jwt.encode({'session_id': -1, 'user_id': -1}, SECRET, algorithm='HS256')
 
-def test_dm_details_invalid_dm_id(user1):
-    # invalid_dmID = -2
-    # response = requests.get(f"{config.url}dm/details/v1", params = {'token': user1[token], 'dm_id': invalid_dmID})
-    # assert response.status_code == 400
-    pass
 
-def test_dm_details_not_in_dm(user1, user2, user3):
-    dm1 = dm_create_v1(user1[token], [user2[AuID]])
-    response = requests.get(f"{config.url}dm/details/v1", params = {'token': user3[token], 'dm_id': dm1[dmID]})
+def test_http_dm_details_valid(user1, user2):
+    dmResponse = requests.post(f"{url}dm/create/v1", json={
+        "token": user1[token],
+        "u_ids": [user2[AuID]]
+    })
+    dm1 = dmResponse.json()
+    expected = {
+        Name: 'user1, user2',
+        'members': [{
+            uID: user1[AuID], 
+            fName: "User",
+            lName: '1',
+            'email': 'first@gmail.com',
+            handle: 'user1',
+        }, {
+            uID: user2[AuID], 
+            fName: "User",
+            lName: '2',
+            'email': 'second@gmail.com',
+            handle: 'user2',
+        }
+        ]
+    }
+    responseUser1 = requests.get(f"{url}dm/details/v1", params = {'token': user1[token], 'dm_id': dm1[dmID]})
+    responseUser2 = requests.get(f"{url}dm/details/v1", params = {'token': user2[token], 'dm_id': dm1[dmID]})
+
+    assert responseUser1.json() == expected
+    assert responseUser2.json() == expected
+
+def test_http_dm_details_invalid_dm_id(user1):
+    invalid_dmID = -2
+    response = requests.get(f"{url}dm/details/v1", params = {'token': user1[token], 'dm_id': invalid_dmID})
+    assert response.status_code == 400
+
+def test_http_dm_details_not_in_dm(user1, user2, user3):
+    responseDM = requests.post(f"{url}dm/create/v1", json={
+        "token": user1[token],
+        "u_ids": [user2[AuID]]
+    })
+    dm1 = responseDM.json()
+    response = requests.get(f"{url}dm/details/v1", params = {'token': user3[token], 'dm_id': dm1[dmID]})
+    
     assert response.status_code == 403
 
-def test_dm_list():
-    pass
+def test_http_dm_list_none(user1):
+    response = requests.get(f"{url}dm/list/v1", params = {'token': user1[token]})
+    assert response.json() == {'dms': []}
 
-def test_dm_create(user1, user2):
-    dmResponse = requests.post(f"{config.url}dm/create/v1", json={
-        'token': user1[token],
-        'u_ids': [user2[AuID]]
+def test_http_dm_list(user1, user2, user3):
+    requests.post(f"{url}dm/create/v1", json={
+        "token": user1[token],
+        "u_ids": [user2[AuID]]
+    })
+    dmResponse = requests.post(f"{url}dm/create/v1", json={
+        "token": user1[token],
+        "u_ids": [user3[AuID]]
+    })
+
+    dm2 = dmResponse.json()
+    response = requests.get(f"{url}dm/list/v1", params = {'token': user3[token]})
+    assert response.json() == {'dms': [{
+        dmID: dm2[dmID],
+        Name: 'user1, user3'
+    }]}
+
+def test_http_dm_create(user1, user2):
+    dmResponse = requests.post(f"{url}dm/create/v1", json={
+        "token": user1[token],
+        "u_ids": [user2[AuID]]
     })
 
     assert dmResponse.json() == {
         dmID: 0,
         'dm_name': 'user1, user2',
     }
-    pass
+
+def test_http_dm_create_invalid_u_ids(user1):
+    invalid_u_id = -1
+    dmResponse = requests.post(f"{url}dm/create/v1", json={
+        "token": user1[token],
+        "u_ids": [invalid_u_id]
+    })
+
+    assert dmResponse.status_code == 400
 
 def test_dm_remove():
     pass
@@ -96,4 +154,7 @@ def test_dm_leave():
     pass
 
 def test_dm_messages():
+    pass
+
+def test_http_dm_invalid_user(invalid_token):
     pass
--- src/server.py
+++ src/server.py
@@ -41,27 +41,23 @@ def clear():
 
 @APP.route("/auth/register/v2", methods=['POST'])
 def auth_register():
-    print(src.data.users)
     payload = request.get_json()
-    return dumps(
-        src.auth.auth_register_v2(payload['email'], payload['password'], payload['name_first'], payload['name_last'])
-    )
+    return src.auth.auth_register_v2(payload['email'], payload['password'], payload['name_first'], payload['name_last'])
 
 @APP.route("/dm/details/v1", methods=['GET'])
 def dm_details():
     token, dm_id = request.args.get('token'), request.args.get('dm_id')
-    return dumps(src.dm.dm_details_v1(token, dm_id))
+    return src.dm.dm_details_v1(token, dm_id)
 
 @APP.route("/dm/list/v1", methods=['GET'])
 def dm_list():
     token = request.args.get('token')
-    return dumps(src.dm.dm_list_v1(token))
+    return src.dm.dm_list_v1(token)
 
 @APP.route("/dm/create/v1", methods=['POST'])
 def dm_create():
     payload = request.get_json()
-    print(src.data.users)
-    return dumps(src.dm.dm_create_v1(payload.get('token'), payload.get('u_ids')))
+    return src.dm.dm_create_v1(payload.get('token'), payload.get('u_ids'))
 
 @APP.route("/dm/remove/v1", methods=['DELETE'])
 def dm_remove():
@@ -81,11 +77,8 @@ def dm_messages():
 
 @APP.route("/auth/login/v2", methods=['POST'])
 def auth_login():
-    print(src.data.users)
     payload = request.get_json()
-    return dumps(
-        src.auth.auth_login_v2(payload['email'], payload['password'])
-    )
+    return src.auth.auth_login_v2(payload['email'], payload['password'])
 
 if __name__ == "__main__":
     APP.run(port=config.port) # Do not edit this port
