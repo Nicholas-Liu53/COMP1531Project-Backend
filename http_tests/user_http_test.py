867e1fa7bd4a691a357080108e9bb29b8befbd35
z5311921 <z5311921@cse.unsw.edu.au>
2021-04-04 14:52:46 +1000

    Finished http testip testing for all functions for user

--- http_tests/user_http_test.py
+++ http_tests/user_http_test.py
@@ -9,24 +9,210 @@ from src.other import clear_v1
 from jwt import encode
 
 SECRET = "MENG"
-'''
+
 def test_http_user_profile_valid():
     requests.delete(f"{url}clear/v1")
     requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
     requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
     token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
-    r = requests.get(f"{url}user/profile/v2", param={'token': token, 'u_id': 0})
-    payload = r.json()
-    assert payload['user']['uid'] == 0
-    assert payload['user']['email'] == 'caricoleman@gmail.com'
-    assert payload['user']['name_first'] == 'cari'
-    assert payload['user']['name_last'] == 'coleman'
-    assert payload['user']['handle_string'] == 'caricoleman'
+    response = requests.get(f"{url}user/profile/v2", param={'token': token, 'u_id': 0})
+    payload = response.json()
+    assert payload == { 
+        'user':
+            {
+            'u_id': 0, 
+            'email': "caricoleman@gmail.com", 
+            'name_first': 'cari', 
+            'name_last': 'coleman', 
+            'handle_string': 'caricoleman'
+            }
+    }
     
 def test_http_user_profile_invalid_uid():
     requests.delete(f"{url}clear/v1")
     requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
     requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
     token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
-    requests.get(f"{url}user/profile/v2", param={'token': token, 'u_id': 1})
-'''
+    response = requests.get(f"{url}user/profile/v2", param={'token': token, 'u_id': 1})
+    assert response.status_code == 400
+
+def test_http_user_setname_valid():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response_1 = requests.put(f"{url}user/profile/setname/v2", json={'token': token, 'name_first': 'kari', 'name_last': 'koleman'})
+    payload_1 = response_1.json()
+    assert payload_1 == {}
+    response_2 = requests.get(f"{url}user/profile/v2", param={'token': token, 'u_id': 0})
+    payload_2 = response_2.json()
+    assert payload_2 == { 
+        'user':
+            {
+            'u_id': 0, 
+            'email': "caricoleman@gmail.com", 
+            'name_first': 'kari', 
+            'name_last': 'koleman', 
+            'handle_string': 'caricoleman'
+            }
+    }
+
+def test_http_user_setname_invalid_long_first_name():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response = requests.put(f"{url}user/profile/setname/v2", json={'token': token, 'name_first': 'kariiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii', 'name_last': 'koleman'})
+    payload = response.json()
+    assert respons.status_code == 400
+
+def test_http_user_setname_invalid_long_last_name():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response = requests.put(f"{url}user/profile/setname/v2", json={'token': token, 'name_first': 'kari', 'name_last': 'kolemaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan'})
+    payload = response.json()
+    assert respons.status_code == 400
+
+def test_http_user_setname_invalid_no_first_name():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response = requests.put(f"{url}user/profile/setname/v2", json={'token': token, 'name_first': '', 'name_last': 'koleman'})
+    payload = response.json()
+    assert respons.status_code == 400    
+
+def test_http_user_setname_invalid_no_last_name():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response = requests.put(f"{url}user/profile/setname/v2", json={'token': token, 'name_first': 'kari', 'name_last': ''})
+    payload = response.json()
+    assert respons.status_code == 400    
+
+def test_http_user_setemail_valid():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response_1 = requests.put(f"{url}user/profile/setemail/v2", json={'token': token, 'email': 'karicoleman@gmail.com'})
+    payload_1 = response_1.json()
+    assert payload_1 == {}
+    response_2 = requests.get(f"{url}user/profile/v2", param={'token': token, 'u_id': 0})
+    payload_2 = response_2.json()
+    assert payload_2 == {
+        'user':
+        {
+        'u_id': 0, 
+        'email': "karicoleman@gmail.com", 
+        'name_first': 'cari', 
+        'name_last': 'coleman', 
+        'handle_string': 'caricoleman'
+        }
+    }
+
+def test_http_user_setemail_invalid_email():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response = requests.put(f"{url}user/profile/setemail/v2", json={'token': token, 'email': 'karicoleman.com'})
+    payload = response.json()
+    assert respons.status_code == 400    
+
+def test_http_user_setemail_invalid_email_in_use():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token_1 = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    requests.post(f"{url}auth/register/v2", json={'email': "eicamondy@gmail.com", "password": "1234567", "name_first": "erica", "name_last": "mondy"})
+    requests.post(f"{url}auth/login/v2", json={'email': "ericamondy@gmail.com", "password": "1234567"})
+    token_2 = encode({'session_id': 1, 'user_id': 1}, SECRET, algorithm='HS256')
+    requests.put(f"{url}user/profile/setemail/v2", json={'token': token_1, 'email': 'karicoleman.com'})
+    reponse = requests.put(f"{url}user/profile/setemail/v2", json={'token': token_2, 'email': 'karicoleman.com'})
+    payload = response.json()
+    assert respons.status_code == 400    
+
+def test_http_user_sethandle_valid():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    reponse_1 = requests.put(f"{url}user/profile/sethandle/v2", json={'token': token, 'handle_string': 'karikoleman'})
+    payload_1 = response_1.json()
+    assert payload_1 == {}
+    response_2 = requests.get(f"{url}user/profile/v2", param={'token': token, 'u_id': 0})
+    payload_2 = response_2.json()
+    assert payload_2 == {
+        'user':
+        {
+        'u_id': 0, 
+        'email': "caricoleman@gmail.com", 
+        'name_first': 'cari', 
+        'name_last': 'coleman', 
+        'handle_string': 'karikoleman'
+        }
+    }
+
+def test_http_user_sethandle_invalid_short_handle():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response = requests.put(f"{url}user/profile/sethandle/v2", json={'token': token, 'handle_string': 'cc'})
+    assert response.status_code == 400    
+
+def test_http_user_sethandle_invalid_long_handle():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response = requests.put(f"{url}user/profile/sethandle/v2", json={'token': token, 'handle_string': 'cariiiiiiiiiiiiiiiiii'})
+    assert response.status_code == 400    
+
+def test_http_user_sethandle_invalid_handle_in_use():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token_1 = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    response_1 = requests.put(f"{url}user/profile/sethandle/v2", json={'token': token_1, 'handle_string': 'kari'})
+    payload_1 = response_1.json()
+    assert payload_1 == {}
+    requests.post(f"{url}auth/register/v2", json={'email': "ericamondy@gmail.com", "password": "1234567", "name_first": "erica", "name_last": "mondy"})
+    requests.post(f"{url}auth/login/v2", json={'email': "ericamondy@gmail.com", "password": "1234567"})
+    token_2 = encode({'session_id': 1, 'user_id': 1}, SECRET, algorithm='HS256')
+    response_2 = requests.put(f"{url}user/profile/sethandle/v2", json={'token': token_2, 'handle_string': 'kari'})
+    assert response_2.status_code == 400
+
+def test_http_users_all_valid():
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
+    requests.post(f"{url}auth/register/v2", json={'email': "ericamondy@gmail.com", "password": "1234567", "name_first": "erica", "name_last": "mondy"})
+    requests.post(f"{url}auth/login/v2", json={'email': "ericamondy@gmail.com", "password": "1234567"})
+    response = requests.get(f"{url}users/all/v1", param={'token': token,})
+    payload = response.json():
+    assert payload == {
+            'users':
+            [{
+            'u_id': 0, 
+            'email': "caricoleman@gmail.com", 
+            'name_first': 'cari', 
+            'name_last': 'coleman', 
+            'handle_string': 'caricoleman'
+            },
+            {
+            'u_id': 1, 
+            'email': "ericamondy@gmail.com", 
+            'name_first': 'erica', 
+            'name_last': 'mondy', 
+            'handle_string': 'ericamondy'
+            }]
+    } 
+    
+
+
