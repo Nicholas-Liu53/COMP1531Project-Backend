017bda7a792f6f3e4a248089878169fcf6c43beb
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-04-04 10:15:25 +1000

    auth.py fully http implemented

--- http_tests/auth_http_test.py
+++ http_tests/auth_http_test.py
@@ -7,78 +7,86 @@ from src.auth import auth_login_v2, auth_register_v2
 from src.other import clear_v1
 from jwt import encode
 
+import src.data
+
+#400 for InputError    
+#403 for AccessError
+
 SECRET = "MENG"
 
 def test_http_auth_login_valid():
-    clear_v1()
-    requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
-    r = requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "1234567"})
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    r = requests.post(f"{url}auth/login/v2", json={"email": "caricoleman@gmail.com", "password": "1234567"})
     token = encode({'session_id': 1, 'user_id': 0}, SECRET, algorithm='HS256')
     payload = r.json()
     assert payload['token'] == token
     assert payload['auth_user_id'] == 0
 
 def test_http_auth_login_invalid_email():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
-        requests.post(f"{url}auth/login/v2", json={'email': "caricoleman.com", "password": "1234567"})
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    response = requests.post(f"{url}auth/login/v2", json={"email": "caricoleman.com", "password": "1234567"})
+    assert response.status_code == 400
 
 def test_http_auth_login_invalid_not_registered_email():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
-        requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@yahoo.com", "password": "1234567"})
+    requests.delete(f"{url}clear/v1")
+    responseOne = requests.post(f"{url}auth/login/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    assert responseOne.status_code == 400
+
+    responseTwo = requests.post(f"{url}auth/login/v2", json={"email": "caricoleman@yahoo.com", "password": "1234567"})
+    assert responseTwo.status_code == 400
 
 def test_http_auth_login_invalid_incorrect_password():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
-        requests.post(f"{url}auth/login/v2", json={'email': "caricoleman@gmail.com", "password": "789101112"})
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    response = requests.post(f"{url}auth/login/v2", json={"email": "caricoleman@gmail.com", "password": "789101112"})
+    assert response.status_code == 400
 
 def test_http_auth_register_valid():
-    clear_v1()
-    r = requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.delete(f"{url}clear/v1")
+    print(src.data.users)
+    r = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
     token = encode({'session_id': 0, 'user_id': 0}, SECRET, algorithm='HS256')
     payload = r.json()
-    assert payload['token'] == token
+    assert payload["token"] == token
     assert payload['auth_user_id'] == 0
 
 def test_http_auth_register_invalid_email():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    requests.delete(f"{url}clear/v1")
+    response = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    assert response.status_code == 400
 
 def test_http_auth_register_invalid_email_in_use():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "erica", "name_last": "mondy"})
-            
+    requests.delete(f"{url}clear/v1")
+    requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "cari", "name_last": "coleman"})
+    response = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "erica", "name_last": "mondy"})
+    assert response.status_code == 400
+
 def test_http_auth_register_invalid_password():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "123", "name_first": "cari", "name_last": "coleman"})
+    requests.delete(f"{url}clear/v1")
+    response = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "123", "name_first": "cari", "name_last": "coleman"})
+    assert response.status_code == 400
 
 def test_http_auth_register_invalid_empty_first_name():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "", "name_last": "coleman"})
+    requests.delete(f"{url}clear/v1")
+    response = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "", "name_last": "coleman"})
+    assert response.status_code == 400
 
 def test_http_auth_register_invalid_long_first_name():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "cariiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", "name_last": "coleman"})
+    requests.delete(f"{url}clear/v1")
+    response = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "cariiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", "name_last": "coleman"})
+    assert response.status_code == 400
 
 def test_http_auth_register_invalid_empty_last_name():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "", "name_last": ""})
+    requests.delete(f"{url}clear/v1")
+    response = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "", "name_last": ""})
+    assert response.status_code == 400
 
 def test_http_auth_register_invalid_long_last_name():
-    clear_v1()
-    with pytest.raises(InputError):
-        requests.post(f"{url}auth/register/v2", json={'email': "caricoleman@gmail.com", "password": "1234567", "name_first": "", "name_last": "colemaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan"})
+    requests.delete(f"{url}clear/v1")
+    response = requests.post(f"{url}auth/register/v2", json={"email": "caricoleman@gmail.com", "password": "1234567", "name_first": "", "name_last": "colemaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan"})
+    assert response.status_code == 400
 
 
 
